d49b6bad31c0104ea07747f9838c519f8c681a3d
james <jamesal1@uci.edu>
2023-05-22 04:40:53 -0400

    changed to numbers

--- color/contentScript.js
+++ color/contentScript.js
@@ -1,5 +1,36 @@
 // contentScript.js
 
+var colors = [
+  "Green",
+  "Turquoise",
+  "DodgerBlue",
+  "MediumPurple",
+  "HotPink",
+  "Red",
+  "MediumVioletRed",
+  "MediumSeaGreen",
+  "Salmon",
+  "Tomato",
+  "Coral",
+  "Orange",
+  "LimeGreen",
+  "DeepSkyBlue",
+  "LightPink",
+  "Orchid",
+  "DarkOrange",
+  "MediumSpringGreen"
+];
+
+function parseStringToInt(inputString) {
+  const parsedInt = parseInt(inputString, 10);
+  
+  if (!isNaN(parsedInt) && parsedInt >= 0 && parsedInt <= 17) {
+    return parsedInt;
+  } else {
+    return -1;
+  }
+}
+
 function handleTextNodes(node) {
     if (node.nodeType === Node.TEXT_NODE) {
       const parent = node.parentNode;
@@ -13,12 +44,12 @@ function handleTextNodes(node) {
           const preText = document.createTextNode(fullTextContent.substring(lastIndex, match.index));
           parent.insertBefore(preText, node);
   
-          const colorValue = match[1];
-          const isValidColor = CSS.supports('color', colorValue);
+          const colorValue = parseStringToInt(match[1]);
+          
   
-          if (isValidColor) {
+          if (colorValue>-1) {
             const colorSpan = document.createElement("span");
-            colorSpan.style.color = colorValue;
+            colorSpan.style.color = colors[colorValue];
             colorSpan.innerText = match[2];
             parent.insertBefore(colorSpan, node);
           }
@@ -30,7 +61,10 @@ function handleTextNodes(node) {
         parent.insertBefore(postText, node);
         parent.removeChild(node);
       }
-    } else if (node.nodeType === Node.ELEMENT_NODE) {
+    } else if (node.getAttribute("data-role") === "user") {
+		console.log("skipped", node);
+	}
+    else if (node.nodeType === Node.ELEMENT_NODE) {
       node.childNodes.forEach(childNode => {
         handleTextNodes(childNode); // Recursively process child nodes
       });
